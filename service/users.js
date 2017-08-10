c3975bdd9b474e1bfcd9a74db49c1d7cbba7d905
Eirik Sander-Fjeld <eirik.sander-fjeld@bouvet.no>
2017-08-10 09:29:40 +0200

    Lagt til token som env variabel.

--- service/users.js
+++ service/users.js
@@ -1,30 +1,33 @@
 var request = require('request');
 var userarray = [];
+var token = process.env.CVPToken;
 exports.GetUsers = function(res) {
     
-    var url = '';
+    var url = 'https://bouvet.cvpartner.com/api/v1/users?offset=';
     var offset = 0;
     var options = {
-        url: url,
+        uri: url+offset,
         headers: {
-            ''
+            'Authorization': 'Token token="${token}"'
         }
     };
 
     function loop() {
         var offsetempty = false;
         if(!offsetempty) {
-            
+            console.log(offsetempty)
             request(options, callback);
-            loop();
+            console.log(userarray.length);
         }
         console.log(userarray.length);
     }
 
     function callback(error, response, body) {
-        console.log("callback");
+        console.log("error: " +error);
+        console.log(response);
+        
         if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
+            var info = JSON.parse(response);
             console.log(info.length);
             if(info.length > 0) {
                 userarray.push(info);
