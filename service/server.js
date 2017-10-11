4e3365f018a1b6318ba54830222ab75e0d7e2a25
Trond <trondtufte89@gmail.com>
2017-10-11 10:17:48 +0200

    added functionality for retrieving the various skills listed

--- service/server.js
+++ service/server.js
@@ -7,38 +7,49 @@ var users = require('./users');
 var customers = require('./customers');
 var references = require('./references');
 var technology = require('./technology');
+var skills = require('./skills');
 
+//Getting the various skills listed in CV-partner
+app.route('/skills').get(function(req, res) {
+  console.log("HTTP GET on /skills");
+  skills.getSkills(res);
+});
 
+//Getting technology categories e.g. front-end, databases etc
 app.route('/tech').get(function(req, res) {
-  console.log("HTTP GET on /users");
+  console.log("HTTP GET on /tech");
   technology.getTechnologyCategories(res);
 });
 
+//Getting registered users in cv-partner
 app.route('/users').get(function(req, res) {
   console.log("HTTP GET on /users");
   users.GetUsers(res);
 });
 
+//Getting the user's cv from cv-partner
 app.route('/cv').get(function(req, res) {
   console.log("HTTP GET on /cv");
   users.GetUserCv(req, res);
 });
 
+//Getting the company's customers
 app.route('/customers').get(function(req, res) {
   console.log("HTTP GET on /customers");
   customers.GetCustomers(res);
 });
 
+//Getting the company's references
 app.route('/references').get(function(req, res) {
   console.log("HTTP GET on /references");
   references.GetAllReferences(res);
 });
 
+//Getting the company's projects
 app.route('/projects').get(function(req, res) {
   console.log("HTTP GET on /projects");
   references.GetCustomerProject(res);
 });
-
 
 
 app.listen(5000, function () {
---
+++ service/skills.js
@@ -0,0 +1,37 @@
+var request = require('request');
+var token = process.env.CVPToken;
+var rp = require('request-promise');
+var skills = [];
+
+
+  exports.getSkills = function(res) {
+    categories = [];
+    var url = 'https://bouvet.cvpartner.com/api/v1/masterdata/technologies/tags?offset=0&limit=5000';
+    var options = {
+        uri: url,
+        headers: {
+            'Authorization': 'Token token=' + token
+        }
+    };
+        rp(options).then(function (content) {
+            var result = JSON.parse(content);
+            Object(result).forEach(function (element, key, _array) {                    
+                skills.push(element);
+             });
+
+            res.writeHead(200, {"Content-Type": "application/json" });
+            res.end(JSON.stringify(skills));
+            console.log("Instances: " + skills.length);
+
+        })
+        .catch(function (err) {       
+            res.status(500);
+            res.end("No Data");  
+        });    
+
+
+        return skills;
+    }
+    
+
+  
